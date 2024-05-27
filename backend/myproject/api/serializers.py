8fa96cba42dc6b4e84479378affcb1c9ae19ddd1
sivajisj2 <sjsjsjsivaji@gmail.com>
2024-05-27 11:17:39 +0000

    wrapped backend

--- backend/myproject/api/serializers.py
+++ backend/myproject/api/serializers.py
@@ -15,5 +15,5 @@ class UserSerializer(serializers.ModelSerializer):
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ['id', 'title', 'content', 'created_at', 'author']
+        fields = ['id', 'title', 'content', 'created', 'author']
         extra_kwargs = {'author': {'read_only':True}}
--- backend/myproject/myproject/urls.py
+++ backend/myproject/myproject/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('api/users/register/',CreateUserView.as_view(),name='register'),
     path('api/token', TokenObtainPairView.as_view(),name='get_token'),
     path('api/token/refresh', TokenRefreshView.as_view(),name='refresh'),
-    path('api-auth/token', include('rest_framework.urls'))
+    path('api-auth/token', include('rest_framework.urls')),
+    path('api/', include('api.urls'))
 ]
