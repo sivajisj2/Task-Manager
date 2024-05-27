e4ae7472c10c4b0c983c965151d160ac75eabdd0
sivajisj2 <sjsjsjsivaji@gmail.com>
2024-05-27 07:32:40 +0000

    created new Note model

--- backend/myproject/api/models.py
+++ backend/myproject/api/models.py
@@ -7,7 +7,8 @@ class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User,on_delete=models.CASCADE, blank=True, null=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE, blank=True, null=True, related_name='notes')
+    
 
     def __str__(self):
         return self.title
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 cd backend/myproject ; source env/bin/activate ; python manage.py runserver;
+
+git add .;
